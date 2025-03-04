7e29dd00c81779019bdab9536beca7d97d72eff0
Washington Luiz de Mello <wasluiz99@gmail.com>
2025-03-04 00:21:09 -0300

    Primeiro commit projeto angular

---
+++ Back/src/Projeto_Angular.API/Controllers/Evento.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Projeto_Angular.API.Models;
+
+namespace Projeto_Angular.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public IEnumerable<Evento> _evento = new Evento[]{
+                new Evento(){
+                EventoId = 1,
+                Tema = "Angular",
+                Local = "Sao paulo",
+                Lote = "1 lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                ImagemURL = "foto1.png"
+                },
+                new Evento(){
+                EventoId = 2,
+                Tema = "Dot NET",
+                Local = "Campinas",
+                Lote = "2 lote",
+                QtdPessoas = 200,
+                DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
+                ImagemURL = "foto2.png"
+                }
+            };
+
+        public EventoController()
+        {           
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetByID(int id)
+        {
+            return _evento.Where(x => x.EventoId == id);
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "POST";
+        }
+    }
+}
