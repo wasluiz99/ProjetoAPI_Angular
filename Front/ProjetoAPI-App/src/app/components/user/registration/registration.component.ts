f858a33b78b72346fe8e5cfbb17bc86ec090f9fe
wasluiz99 <wasluiz99@gmail.com>
2025-03-17 21:14:38 -0300

    Reactive Forms e Mudança de Layout

--- Front/ProjetoAPI-App/src/app/components/user/registration/registration.component.ts
+++ Front/ProjetoAPI-App/src/app/components/user/registration/registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorField } from '@app/helpers/ValidatorField';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+    form!: FormGroup;
+  
+    get f(): any {
+      return this.form.controls;
+    }
+    
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.validation();
+  }
+
+
+  public validation(): void {
+
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMatch('senha', 'confirmarSenha')
+    };
+
+
+    this.form = this.fb.group({
+      primeiroNome: ['', [Validators.required]],
+      ultimoNome: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      usuario: ['', [Validators.required]],
+      senha: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(15)]],
+      confirmarSenha: ['', Validators.required],
+    }, formOptions);
   }
 
 }
